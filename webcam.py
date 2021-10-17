b7160672885d5895810f0021322e55d36e5d4126
AbdJR <abdallah.afifi@hotmail.com>
2021-10-17 11:30:44 +0300

    Made The System Work and Connected it to RPi

--- webcam.py
+++ webcam.py
@@ -1,6 +1,6 @@
-# import cv2
-# import prediction
-# import socket
+import cv2
+import prediction
+import socket
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -17,6 +17,15 @@ def update_balance(uid, balance):
     db.reference('Customer').child(uid).update({
         'balance': balance,
     })
+def convert_to_money(sbalance):
+    sbalance = sbalance[1:]
+    if sbalance == '05':
+        return 0.5
+    return int(sbalance)
+def screenshot():
+    global cam
+    cv2.imwrite('screenshot.png', cam.read()[1])
+
 '''
 #Creating the database and adding content
 ref = db.reference('/')
@@ -70,17 +79,16 @@ ref.child("test").set({
 ref = db.reference('Customer').child('test')
 ref.delete()
 '''
-'''
 
-ip = "192.168.1.29"
+
+ip = "192.168.1.12"
 #
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((ip, 8080))
+msg = "connected"
+client.send(msg.encode())
 print("Client: Connected")
 
-def screenshot():
-    global cam
-    cv2.imwrite('screenshot.png', cam.read()[1])
 
 
 with open('labels.txt','r') as f:
@@ -92,34 +100,39 @@ with open('labels.txt','r') as f:
 		m=listl.append(listli)
 	print(listl)
 
-# cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(1)
 
 
 while True:
-    # sleep(1)
+    sleep(1)
     cam.release()
     from_server = client.recv(4096).decode()
     while from_server.split()[0] != "detected":
         from_server = client.recv(4096).decode()
+    id = int(from_server.split[1])
+
+    print("Please Enter The Money you want to Enter, and After Each Entry Press \'Space\'")
+    print("Press \'ESC\' When You Are Done")
     cam = cv2.VideoCapture(1)
     ret, img = cam.read()
 
     cv2.imshow('My camera',img)
+    while True:
+        ch = cv2.waitKey(5)
+        if ch == 27:    #ESC
+            break
+        elif ch == 32:  #SPACE
+            screenshot()
+        result = prediction.predict('screenshot.png')
 
-    ch = cv2.waitKey(5)
-    if ch == 27:
-        break
-    screenshot()
-
-    result = prediction.predict('screenshot.png')
-
-    print(listl[result][1])
-
-        # exit
+        if(listl[result][1] in {'s05','s1','s2','s5','s10','s20','s50','s100','s200'}):
+            update_balance(id,convert_to_money(listl[result][1]))
+        else:
+            print("Invalid Curreny, Please Try Again")
+    print("Thank  You For Using Our Banking System, Have a Nice Day!")
 
 
 cv2.destroyAllWindows()
 
+client.close()
 
-
-'''
